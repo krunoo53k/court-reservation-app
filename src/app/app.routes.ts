73fa61a5b61d763a054a517ea2ec392874a8ded1
kruno <petrik.kpetrik@gmail.com>
2024-11-20 17:23:41 +0100

    feat: base routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { CourtReservationComponent } from "./components/court-reservation/court-reservation.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "/courts", pathMatch: "full" },
+  { path: "courts", component: CourtReservationComponent },
+];
