f25d6084412c2c3fa2b651ab931cfc8206ae5ea4
kruno <petrik.kpetrik@gmail.com>
2024-11-21 14:05:26 +0100

    feat: login integrated into header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { AuthService } from "../../services/auth.service";
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, ButtonModule],
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(public authService: AuthService) {}
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
