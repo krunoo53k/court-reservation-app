49a56b1d10745bd46bcf9199ee69fe1f4a23a163
kruno <petrik.kpetrik@gmail.com>
2024-11-26 14:06:25 +0100

    feat: buttons to iterate through dates

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,11 +1,12 @@
 import { Component, Output, EventEmitter } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { CalendarModule } from "primeng/calendar";
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: "app-calendar",
   standalone: true,
-  imports: [CalendarModule, FormsModule],
+  imports: [CalendarModule, FormsModule, ButtonModule],
   templateUrl: "./calendar.component.html",
   styleUrl: "./calendar.component.css",
 })
@@ -16,5 +17,31 @@ export class CalendarComponent {
 
   onDateSelect(event: any) {
     this.dateChange.emit(this.date);
+  }
+
+  nextDay() {
+    const nextDate = new Date(this.date);
+    nextDate.setDate(nextDate.getDate() + 1);
+    this.date = nextDate;
+    this.dateChange.emit(this.date);
+  }
+
+  previousDay() {
+    const prevDate = new Date(this.date);
+    prevDate.setDate(prevDate.getDate() - 1);
+    if (prevDate >= this.minDate) {
+      this.date = prevDate;
+      this.dateChange.emit(this.date);
+    }
+  }
+
+  isPreviousDayDisabled(): boolean {
+    const currentDate = new Date(this.date);
+    const minDate = new Date(this.minDate);
+
+    currentDate.setHours(0, 0, 0, 0);
+    minDate.setHours(0, 0, 0, 0);
+
+    return currentDate.getTime() <= minDate.getTime();
   }
 }
